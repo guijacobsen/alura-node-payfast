c8700682138042b2742905236a0ca229d78ea7b0
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-01-31 14:23:01 -0200

    [WIP] Implementado codigo soap

--- payfast/servicos/correiosSOAPClient.js
+++ payfast/servicos/correiosSOAPClient.js
@@ -1,18 +1,22 @@
 var soap = require('soap');
-var url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
-soap.createClient(url, (erro, cliente) => {
-    if( erro ) {
-        console.log('Falha ao criar cliente soap correios');
-        console.log('erro ::', erro);
-    } else {
-        console.log('Cliente soap correios criado');
 
-        cliente.CalcPrazo({
-            'nCdServico' : '40010',
-            'sCepOrigem' : '04101300',
-            'sCepDestino' : '65000600'
-        }, (err, resultado) => {
-            console.log(JSON.stringify(resultado));
-        });
-    }
-});
+function CorreiosSOAPClient() {
+    this._url = 'http://ws.correios.com.br/calculador/CalcPrecoPrazo.asmx?wsdl';
+}
+
+CorreiosSOAPClient.prototype.calculaPrazo = function(args, callback) {
+
+    soap.createClient(this._url, (erro, cliente) => {
+        if( erro ) {
+            console.log('Falha ao criar cliente soap correios');
+            console.log('erro ::', erro);
+        } else {
+            console.log('Cliente soap correios criado');    
+            cliente.CalcPrazo(args, callback);
+        }
+    });
+
+}
+
+
+module.exports = () => {return CorreiosSOAPClient;}
