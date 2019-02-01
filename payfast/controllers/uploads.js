0f09eadd230c968ae01197840caccc34d0ccfb5a
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-02-01 11:59:33 -0200

    [WIP] Upload file com stream

---
+++ payfast/controllers/uploads.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+module.exports = (app) => {
+
+    /** 
+     *
+    **/
+    
+    app.post('/upload/imagem', (req, res) => {
+        console.log('Recebendo requisicao post p/ upload de imagem');
+
+        // var body = req.body; // nao funiona, pois os dados sao recebidos em stream
+        var filename = req.headers.filename;
+        req.pipe(fs.createWriteStream(`files/${filename}`)).on('finish', () => {
+            console.log('arquivo escrito');
+            res.status(201).send('ok');
+        });
+
+    });
+
+}
