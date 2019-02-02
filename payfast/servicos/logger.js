9cc245dc730e883865f1ad9fb4eb208a91d81b7a
Guilherme Jacobsen <guijacobsen@gmail.com>
2019-02-01 23:38:09 -0200

    [WIP] Adicionado timestamp logger

--- payfast/servicos/logger.js
+++ payfast/servicos/logger.js
@@ -1,17 +1,28 @@
 var winston = require('winston');
-
+var formatL = winston.format;
 // var logger = new winston.Logger({
 
 module.exports = winston.createLogger({
     // format: winston.format.json(),
     // level: 'info',
     // format: winston.format.json(),
+    // format: formatL.combine(
+    //     formatL.timestamp({
+    //         format: 'YYYY-MM-DD HH:mm:ss'
+    //     })
+    // ),
     transports: [
         new winston.transports.File({
             level: 'info',
             filename: "logs/payfast.log",
             maxsize: 1000,
-            maxFiles: 10
+            maxFiles: 10,
+            format: formatL.combine(
+                formatL.timestamp({
+                    format: 'YYYY-MM-DD HH:mm:ss'
+                }),
+                formatL.json()
+            )
         })
     ]
 });
